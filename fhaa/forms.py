05bc9a8f73627472cf19859142481888f123f65c
jun-el <junlee9834@gmail.com>
2023-06-06 18:55:11 +0900

    jlee

--- fhaa/forms.py
+++ fhaa/forms.py
@@ -1,15 +1,24 @@
+"""
+Forms 
+Authors: jlee (junlee9834@gmail.com)
+"""
+
 from flask_wtf import FlaskForm # pip install flask-wtf
 from wtforms import StringField, TextAreaField, PasswordField, EmailField, DateField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
+
 class UserCreateForm(FlaskForm):
     """환자 회원가입 폼 \n
-    `email` : email, pk \n
-    `password1` : password \n
-    `password2` : verify password \n
-    `nickname` : nickname \n
-    `birth` : birthday \n
-    `phone` : phone number \n
+    Field:
+        `email` : email, pk \n
+        `password1` : password 
+        `password2` : verify password \n
+        `nickname` : nickname \n
+        `birth` : birthday \n
+        `phone` : phone number \n
+    Authors: jlee (junlee9834@gmail.com)         
+    
     """
     email = EmailField('이메일', validators=[DataRequired(), Email('이메일 형식으로 입력하세요.')])
     password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
@@ -22,11 +31,13 @@ class UserCreateForm(FlaskForm):
     
 class HospitalCreateForm(FlaskForm):
     """환자 회원가입 폼 \n
-    `crn` : Company Registration Number, pk \n
-    `nickname` : nickname \n
-    `password1` : password \n
-    `password2` : verify password \n
-    `phone` : phone number
+    Field:
+        `crn` : Company Registration Number, pk \n
+        `nickname` : nickname \n
+        `password1` : password \n
+        `password2` : verify password \n
+        `phone` : phone number \n
+    Authors: jlee (junlee9834@gmail.com)             
     """
     crn = StringField('사업자등록번호', validators=[DataRequired(), Length(min=2, max=25)])
     password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
--- fhaa/models.py
+++ fhaa/models.py
@@ -1,3 +1,8 @@
+"""
+Models for Database 
+Authors: jlee (junlee9834@gmail.com)
+"""
+
 from fhaa import db
 
 class User(db.Model):
@@ -8,19 +13,22 @@ class User(db.Model):
         `password`: 비밀번호 \n
         `phone`: 핸드폰 \n
         `birth`: 생년월일 \n
+    Authors: jlee (junlee9834@gmail.com)         
     """
     email = db.Column(db.String(), primary_key=True) # 이메일 (PK)    
-    username = db.Column(db.String(), unique=True, nullable=False) # 이름
-    password = db.Column(db.String(), nullable=False) # 비밀번호
-    phone = db.Column(db.String(12), nullable=False) # 핸드폰
+    name = db.Column(db.String(), unique=True, nullable=False) # 이름
+    pwd = db.Column(db.String(), nullable=False) # 비밀번호
+    phone = db.Column(db.String(), nullable=False) # 핸드폰
     birth = db.Column(db.Date(), nullable=False) # 생년월일
         
 class Hospital(db.Model):
     """병원 테이블
+    
+    Authors: jlee (junlee9834@gmail.com) 
     """
     crn = db.Column(db.String(), primary_key=True) # 사업자등록번호
     pwd = db.Column(db.String(), nullable=False) # 비밀번호
     dept = db.Column(db.String(), nullable=False) # 의료종목
     name = db.Column(db.String(), nullable=False) # 병원명
     addr = db.Column(db.String(), nullable=False) # 병원주소
-    tel = db.Column(db.String(12), nullable=False) # 전화번호
+    tel = db.Column(db.String(), nullable=False) # 전화번호
