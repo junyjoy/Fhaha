b2b6c0a91ce01de61f3c5af6a697d41fc94f8f45
jun-el <junlee9834@gmail.com>
2023-06-06 16:25:59 +0900

    temp models

--- fhaa/models.py
+++ fhaa/models.py
@@ -1,7 +1,26 @@
 from fhaa import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    """환자 테이블  \n
+    Variables: \n
+        `email`: 이메일 (PK) \n
+        `username`: 이름 \n
+        `password`: 비밀번호 \n
+        `phone`: 핸드폰 \n
+        `birth`: 생년월일 \n
+    """
+    email = db.Column(db.String(), primary_key=True) # 이메일 (PK)    
+    username = db.Column(db.String(), unique=True, nullable=False) # 이름
+    password = db.Column(db.String(), nullable=False) # 비밀번호
+    phone = db.Column(db.String(12), nullable=False) # 핸드폰
+    birth = db.Column(db.Date(), nullable=False) # 생년월일
+        
+class Hospital(db.Model):
+    """병원 테이블
+    """
+    crn = db.Column(db.String(), primary_key=True) # 사업자등록번호
+    pwd = db.Column(db.String(), nullable=False) # 비밀번호
+    dept = db.Column(db.String(), nullable=False) # 의료종목
+    name = db.Column(db.String(), nullable=False) # 병원명
+    addr = db.Column(db.String(), nullable=False) # 병원주소
+    tel = db.Column(db.String(12), nullable=False) # 전화번호
