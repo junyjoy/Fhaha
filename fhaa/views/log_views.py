6116d81fee50078ab57bd98c06c363a8f2389a5b
jun-el <junlee9834@gmail.com>
2023-06-09 14:29:23 +0900

    update navbar

--- fhaa/views/log_views.py
+++ fhaa/views/log_views.py
@@ -13,3 +13,8 @@ def pat_login():
 @bp.route('/hos/', methods=('GET','POST'))
 def hos_login():
     return render_template('log/hos_login.html')
+
+
+@bp.route('/', methods=('GET','POST'))
+def logout():
+    return render_template('log/login.html')
