f500d3092575dd77606407980adfc1614ec6d4a7
hojinuuu <leehojin5233@gmail.com>
2023-06-09 15:22:20 +0900

    qq

--- fhaa/views/change_views.py
+++ fhaa/views/change_views.py
@@ -6,21 +6,36 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 from flask import Blueprint, render_template
 from fhaa import db
+from fhaa.models import User
 
 
 bp = Blueprint('change', __name__, url_prefix='/change')
 
 
 @bp.route('/change')
-def changeH():
-    session['user_id'] = "test@test.com"
+def change():
+    # session['user_id'] = "test@test.com"
     # session['user_type'] = "hospital"
-    session['user_type'] = "patient"
+    # session['user_type'] = "patient"
 
 
-    user_id = session.get('user_type')
-    if user_id == "patient":
+    user_type = session.get('user_type')
+    
+    if user_type == "pat":
         return render_template('change/changeN.html')
-    elif user_id == "hospital":
+    elif user_type == "hos":
         return render_template('change/changeH.html')
+    else:
+        return redirect(url_for('main.index'))
 
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    user_type = session.get('user_type')
+    if user_id is None:
+        g.user = None
+        g.user_type = None
+    else:
+        g.user = User.query.get(user_id)
+        g.user_type = User.query.get(user_type)
