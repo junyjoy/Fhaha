234f135ec6b14f4068414e5a8fec962c6832bf29
hojinuuu <leehojin5233@gmail.com>
2023-06-09 15:06:34 +0900

    a

--- fhaa/views/change_views.py
+++ fhaa/views/change_views.py
@@ -5,16 +5,17 @@ from flask import Blueprint, url_for, render_template, flash, request, session, 
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 from flask import Blueprint, render_template
+from fhaa import db
 
 
 bp = Blueprint('change', __name__, url_prefix='/change')
 
 
-@bp.route('/')
+@bp.route('/change')
 def changeH():
     session['user_id'] = "test@test.com"
-    session['user_type'] = "hospital"
-    # session['user_type'] = "patient"
+    # session['user_type'] = "hospital"
+    session['user_type'] = "patient"
 
 
     user_id = session.get('user_type')
