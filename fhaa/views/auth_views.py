cfaa0a447c983a8d5453642917366e8b19de1959
jun-el <junlee9834@gmail.com>
2023-06-11 14:20:23 +0900

    update auth_view.py

--- fhaa/views/auth_views.py
+++ fhaa/views/auth_views.py
@@ -20,46 +20,53 @@ def signup():
 
 @bp.route('/patient/', methods=('GET', 'POST'))
 def patient_signup():
-    form = UserCreateForm()
-    if request.method == 'POST' and form.validate_on_submit() and g.user is None:
-        user = User.query.filter_by(pat_ema=form.email.data).first()
-        if not user:
-            user = User(pat_ema=form.email.data,
-                        pat_name=form.name.data,
-                        pat_pw=generate_password_hash(form.password1.data),
-                        pat_tel=form.phone.data,
-                        pat_bir=form.birth.data)
-            
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('auth.congrats', _method='POST',user_id=user.pat_ema, user_name=user.pat_name))
-        else:
-            flash('이미 존재하는 사용자입니다.')
-    return render_template('auth/patient_signup.html', form=form)
+    if g.user is None:
+        form = UserCreateForm()
+        if request.method == 'POST' and form.validate_on_submit():
+            user = User.query.filter_by(pat_ema=form.email.data).first()
+            if not user:
+                user = User(pat_ema=form.email.data,
+                            pat_name=form.name.data,
+                            pat_pw=generate_password_hash(form.password1.data),
+                            pat_tel=form.phone.data,
+                            pat_bir=form.birth.data)
+                
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('auth.congrats', _method='POST',user_id=user.pat_ema, user_name=user.pat_name))
+            else:
+                flash('이미 존재하는 사용자입니다.')
+        return render_template('auth/patient_signup.html', form=form)
+    else:
+        return redirect(url_for('main.index'))
+    
 
 
 @bp.route('/hospital/', methods=('GET', 'POST'))
 def hospital_signup():
-    form = HospitalCreateForm()
-    if request.method == 'POST' and form.validate_on_submit() and g.user is None: 
-        user = Hospital.query.filter_by(hos_cid=form.crn.data).first()
-        if not user:
-            print(form.address1.data + form.address2.data)
-            user = Hospital(hos_cid=form.crn.data,
-                        hos_pwd=generate_password_hash(form.password1.data),
-                        hos_name=form.name.data,
-                        hos_addr=form.address1.data + form.address2.data,
-                        hos_tel=form.tel.data,
-                        hos_type=form.type.data)
-            db.session.add(user)
-            db.session.commit()
-            # 가입완료 페이지에서 id와 name을 표시하기 위함
-            session['created_id'] = form.crn.data
-            session['created_name'] = form.name.data
-            return redirect(url_for('auth.congrats', _method='POST', user_id=user.hos_cid, user_name=user.hos_name))
-        else:
-            flash('이미 존재하는 사용자입니다.')
-    return render_template('auth/hospital_signup.html', form=form)
+    if g.user is None:
+        form = HospitalCreateForm()
+        if request.method == 'POST' and form.validate_on_submit() and g.user is None: 
+            user = Hospital.query.filter_by(hos_cid=form.crn.data).first()
+            if not user:
+                print(form.address1.data + form.address2.data)
+                user = Hospital(hos_cid=form.crn.data,
+                            hos_pwd=generate_password_hash(form.password1.data),
+                            hos_name=form.name.data,
+                            hos_addr=form.address1.data + ', ' + form.address2.data,
+                            hos_tel=form.tel.data,
+                            hos_type=form.type.data)
+                db.session.add(user)
+                db.session.commit()
+                # 가입완료 페이지에서 id와 name을 표시하기 위함
+                session['created_id'] = form.crn.data
+                session['created_name'] = form.name.data
+                return redirect(url_for('auth.congrats', user_id=user.hos_cid, user_name=user.hos_name))
+            else:
+                flash('이미 존재하는 사용자입니다.')
+        return render_template('auth/hospital_signup.html', form=form)
+    else:
+        return redirect(url_for('main.index'))
 
 
 @bp.route('/congrats/', methods=('POST','GET'))
