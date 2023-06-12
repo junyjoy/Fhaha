704fc5b2973344c05e7e364b9a47b4c1211cd270
jun-el <junlee9834@gmail.com>
2023-06-12 17:10:57 +0900

    update congrats, signup

--- fhaa/views/auth_views.py
+++ fhaa/views/auth_views.py
@@ -11,6 +11,7 @@ from werkzeug.utils import redirect
 from werkzeug.security import generate_password_hash
 import functools
 
+
 bp = Blueprint('auth', __name__, url_prefix='/signup')
 
 
@@ -23,8 +24,10 @@ def signup():
 def patient_signup():
     if g.user is None:
         form = UserCreateForm()
+        
         if request.method == 'POST' and form.validate_on_submit():
             user = User.query.filter_by(pat_ema=form.email.data).first()
+            # user가 없으면 새로운 user를 생성하고 auth.congrats로 리다이렉트
             if not user:
                 user = User(pat_ema=form.email.data,
                             pat_name=form.name.data,
@@ -35,7 +38,7 @@ def patient_signup():
                 db.session.add(user)
                 db.session.commit()
                 
-                return redirect(url_for('auth.congrats', _method='POST',user_id=user.pat_ema, user_name=user.pat_name))
+                return redirect(url_for('auth.congrats', user_id=form.email.data, user_name=form.name.data), code=307)
             else:
                 flash('이미 존재하는 사용자입니다.')
                 
@@ -43,7 +46,6 @@ def patient_signup():
     
     else:
         return redirect(url_for('main.index'))
-    
 
 
 @bp.route('/hospital/', methods=('GET', 'POST'))
@@ -59,7 +61,6 @@ def hospital_signup():
                             hos_name=form.name.data,
                             hos_addr=form.address1.data + ';block;' + form.address2.data,
                             hos_tel=form.tel.data,
-                            hos_type=form.type.data
                         )
                 db.session.add(user)
                 db.session.commit()
@@ -67,10 +68,10 @@ def hospital_signup():
                 for ill_pid in form.subject.data:
                     hossub = HosSub(hos_cid=user.hos_cid, ill_pid=ill_pid)
                     db.session.add(hossub)
-                    db.session.commit()
+                db.session.commit()
                     
                 # 가입완료 페이지에서 id와 name을 표시하기 위함
-                return redirect(url_for('auth.congrats', user_id=user.hos_cid, user_name=user.hos_name))
+                return redirect(url_for('auth.congrats', user_id=user.hos_cid, user_name=user.hos_name), code=307)
             else:
                 flash('이미 존재하는 사용자입니다.')
                 
@@ -80,24 +81,49 @@ def hospital_signup():
         return redirect(url_for('main.index'))
 
 
-@bp.route('/congrats/', methods=('POST','GET'))
+@bp.route('/congrats', methods=('GET', 'POST'))
 def congrats():
     if request.method == 'POST' :
-        user_id = request.get_data('user_id')
-        user_name = request.get_data('user_name')
+        user_id = request.args.get('user_id')
+        request.get_data()
+        user_name = request.args.get('user_name')
+        print(user_id, user_name)
         return render_template('auth/congrats.html', user_id=user_id, user_name=user_name)
     else:
         return redirect(url_for('main.index'))
-    
 
 
-def login_required(view):
-    """로그인 후 이용할 수 있는 서비스에 사용됨
+def login_required_all(view):
+    """모든 사용자로 로그인 후 이용할 수 있는 서비스에 사용됨
     """
     @functools.wraps(view)
     def wrapped_view(*args, **kwargs):
         if g.user is None:
             _next = request.url if request.method == 'GET' else ''
-            return redirect(url_for('log.login', next=_next))
+            return redirect(url_for('login.login', next=_next))
+        return view(*args, **kwargs)
+    return wrapped_view
+
+
+def login_required_for_patient(view):
+    """환자로 로그인 후 이용할 수 있는 서비스에 사용됨
+    """
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is not None and g.user == "hospital":
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('login.login', next=_next))
+        return view(*args, **kwargs)
+    return wrapped_view
+
+
+def login_required_for_hospital(view):
+    """병원으로 로그인 후 이용할 수 있는 서비스에 사용됨
+    """
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is not None and g.user == "patient":
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('login.login', next=_next))
         return view(*args, **kwargs)
     return wrapped_view
