9ea1d4782110b1e125b5c9c606fd45d7a6617a2a
junyjoy <junyjoy@naver.com>
2023-06-12 17:09:32 +0900

    흠.. main_views, 병원타입에 따른 html 수정

--- fhaa/views/main_views.py
+++ fhaa/views/main_views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from fhaa.models import Hospital, HosSub, Subject
+import sqlalchemy
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -9,36 +11,54 @@ def index():
 
 @bp.route('/im')
 def im():
-    return render_template('main/IM_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="내과").all()
+
+    return render_template('main/IM_adv.html',hoslist=hos)
 
 @bp.route('/der')
 def der():
-    return render_template('main/DER_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="피부과").all()
+
+    return render_template('main/DER_adv.html',hoslist=hos)
 
 @bp.route('/uro')
 def uro():
-    return render_template('main/URO_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="비뇨의학과").all()
+
+    return render_template('main/URO_adv.html',hoslist=hos)
 
 @bp.route('/obgy')
 def obgy():
-    return render_template('main/OBGY_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="산부인과").all()
+
+    return render_template('main/OBGY_adv.html',hoslist=hos)
 
 @bp.route('/oph')
 def oph():
-    return render_template('main/OPH_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="안과").all()
+
+    return render_template('main/OPH_adv.html',hoslist=hos)
 
 @bp.route('/os')
 def os():
-    return render_template('main/OS_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="정형외과").all()
+
+    return render_template('main/OS_adv.html',hoslist=hos)
 
 @bp.route('/ent')
 def ent():
-    return render_template('main/ENT_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="이비인후과").all()
+
+    return render_template('main/ENT_adv.html',hoslist=hos)
 
 @bp.route('/dt')
 def dt():
-    return render_template('main/DT_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="치과").all()
+
+    return render_template('main/DT_adv.html',hoslist=hos)
 
 @bp.route('/pd')
 def pd():
-    return render_template('main/PD_adv.html')
+    hos = Hospital.query.join(HosSub).join(Subject).filter(Hospital.hos_cid==HosSub.hos_cid, Subject.ill_type=="소아과").all()
+
+    return render_template('main/PD_adv.html',hoslist=hos)
