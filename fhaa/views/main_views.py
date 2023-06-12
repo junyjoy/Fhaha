db2ac8f57af81386dc51ae4ff63892b8e1b86c84
junyjoy <junyjoy@naver.com>
2023-06-12 14:23:34 +0900

    advertise page update

--- fhaa/views/main_views.py
+++ fhaa/views/main_views.py
@@ -7,6 +7,38 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def index():
     return render_template('main/main.html')
 
-@bp.route('/adv')
-def adv():
-    return render_template('main/advertise.html')
+@bp.route('/im')
+def im():
+    return render_template('main/IM_adv.html')
+
+@bp.route('/der')
+def der():
+    return render_template('main/DER_adv.html')
+
+@bp.route('/uro')
+def uro():
+    return render_template('main/URO_adv.html')
+
+@bp.route('/obgy')
+def obgy():
+    return render_template('main/OBGY_adv.html')
+
+@bp.route('/oph')
+def oph():
+    return render_template('main/OPH_adv.html')
+
+@bp.route('/os')
+def os():
+    return render_template('main/OS_adv.html')
+
+@bp.route('/ent')
+def ent():
+    return render_template('main/ENT_adv.html')
+
+@bp.route('/dt')
+def dt():
+    return render_template('main/DT_adv.html')
+
+@bp.route('/pd')
+def pd():
+    return render_template('main/PD_adv.html')
