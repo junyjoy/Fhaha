8807d5e37c5e26ccc07803ff83ae8b1867859647
jun-el <junlee9834@gmail.com>
2023-06-13 11:19:22 +0900

    test

--- fhaa/static/address.js
+++ fhaa/static/address.js
@@ -8,3 +8,15 @@ function execDaumPostcode() {
         }
     }).open();
 }
+
+function getDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            var addr = data.address; // 최종 주소 변수
+
+            // 주소 정보를 해당 필드에 넣는다.
+            document.getElementById("location").value = addr;
+        }
+    }).open();
+    setCurrentLocation(document.getElementById("location").value);
+}
