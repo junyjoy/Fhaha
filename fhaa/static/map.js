fc83db85d2a805c7a93189db8c7f3e065a43c01d
jun-el <junlee9834@gmail.com>
2023-06-19 22:46:23 +0900

    trouble shooting for ocp

--- fhaa/static/map.js
+++ fhaa/static/map.js
@@ -11,29 +11,8 @@ var marker;
 
 var location_input = document.getElementById('location');
 
+
 // 현재 위치 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-if (navigator.geolocation) {
-    
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
-        
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
-        
-        var locPosition = new kakao.maps.LatLng(lat, lon) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-        
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition);
-            
-      });
-    
-} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-    
-    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667)
-        
-    displayMarker(locPosition);
-}
 
 // 지도에 마커를 표시하고 중심좌표를 접속위치로 변경하는 함수입니다
 function displayMarker(locPosition) {
@@ -59,6 +38,29 @@ function displayMarker(locPosition) {
         }
     });
 }   
+
+// HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+if (navigator.geolocation) {
+    
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        
+        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition);
+            
+      });
+    
+} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+    
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+        
+    displayMarker(locPosition);
+}
 
 
 // 마커 생성 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -103,8 +105,6 @@ function searchDetailAddrFromCoords(coords, callback) {
     // 좌표로 법정동 상세 주소 정보를 요청합니다
     geocoder.coord2Address(coords['La'], coords['Ma'], callback);
 }
-
-
 
 
 function setLocation() {
